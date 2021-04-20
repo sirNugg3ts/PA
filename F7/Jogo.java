fb1eccc20303bf4b31c6342e155d0ea325033722
Diogo Pascoal <diogopascoal789@gmail.com>
2021-04-20 18:44:06 +0100

    upload jogo e grafico

---
+++ F7/Jogo.java
@@ -0,0 +1,111 @@
+package bolas.logica.dados;
+
+
+import bolas.logica.TipoBola;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Jogo {
+    public static final int NRBRANCAS = 10;
+    public static final int NRPRETAS  = 10;
+    ArrayList<TipoBola> saco;
+    int nrBolasBrancasGanhas,nrBolasPretasFora,nrBolasBrancasFora;
+    int aposta;
+
+    public Jogo() {
+        iniciaJogo();
+    }
+
+    public void iniciaJogo() {
+        saco = new ArrayList<>();
+        saco.addAll(Collections.nCopies(NRBRANCAS,TipoBola.Branca));
+        saco.addAll(Collections.nCopies(NRPRETAS,TipoBola.Preta));
+        nrBolasBrancasGanhas = 0;
+        nrBolasBrancasFora = 0;
+        nrBolasPretasFora = 0;
+        aposta = 0;
+        Collections.shuffle(saco);
+    }
+
+    public int getNrBolasBrancasGanhas() {
+        return nrBolasBrancasGanhas;
+    }
+
+    public int getNrBolasPretasFora() {
+        return nrBolasPretasFora;
+    }
+
+    public int getNrBolasBrancasFora() {
+        return nrBolasBrancasFora;
+    }
+
+    public boolean isSacoVazio() {
+        return saco.isEmpty();
+    }
+
+    public boolean aposta(int bolas) {
+        if (bolas > nrBolasBrancasGanhas) {
+            return false;
+        }
+        nrBolasBrancasGanhas -= bolas;
+        aposta = bolas;
+        return true;
+    }
+
+    public int getAposta() {
+        return aposta;
+    }
+
+    public void perdeAposta() {
+        // nrBolasBrancasGanhas -= aposta; //se não fossem antes retiradas
+    }
+
+    public void recuperaAposta() {
+        nrBolasBrancasGanhas += aposta;
+    }
+
+    public void ganhaBranca() {
+        nrBolasBrancasGanhas += 1;
+    }
+
+    public boolean perdeBranca() {
+        if (nrBolasBrancasGanhas < 1)
+            return false;
+        nrBolasBrancasGanhas--;
+        return true;
+    }
+
+    public void retiraPreta() {
+        nrBolasPretasFora++;
+    }
+
+    public void retiraBranca() {
+        nrBolasBrancasFora++;
+    }
+
+
+    public void devolveBolas(int nrBolas, TipoBola tipo) {
+        if (nrBolas>0)
+            saco.addAll(Collections.nCopies(nrBolas,tipo));
+        Collections.shuffle(saco);
+    }
+
+    public TipoBola tiraBolaDoSaco() {
+        if (saco.isEmpty()) {
+            return TipoBola.none;
+        }
+        return saco.remove(0);
+    }
+
+    @Override
+    public String toString() {
+        return "Jogo{" +
+                "saco=" + saco +
+                ", nrBolasBrancasGanhas=" + nrBolasBrancasGanhas +
+                ", nrBolasPretasFora=" + nrBolasPretasFora +
+                ", nrBolasBrancasFora=" + nrBolasBrancasFora +
+                ", aposta=" + aposta +
+                '}';
+    }
+}
